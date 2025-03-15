08dd77e2ecefb889230c8c50a13c8a7cc9b3b7b0
Eric Brombaugh <ebrombaugh1@cox.net>
2025-03-15 16:44:14 -0700

    Removing unneeded changes to kernel drivers, add backlight control

--- linux_5.10/drivers/staging/fbtft/fbtft-core.c
+++ linux_5.10/drivers/staging/fbtft/fbtft-core.c
@@ -75,8 +75,6 @@ void fbtft_dbg_hex(const struct device *dev, int groupsize,
 }
 EXPORT_SYMBOL(fbtft_dbg_hex);
 
-#if 0
-// original GPIO setup code doesn't use DT
 static int fbtft_request_one_gpio(struct fbtft_par *par,
 				  const char *name, int index,
 				  struct gpio_desc **gpiop)
@@ -92,8 +90,9 @@ static int fbtft_request_one_gpio(struct fbtft_par *par,
 			"Failed to request %s GPIO: %d\n", name, ret);
 		return ret;
 	}
+	
 	fbtft_par_dbg(DEBUG_REQUEST_GPIOS, par, "%s: '%s' GPIO\n",
-		      __func__, name);
+			__func__, name);
 
 	return ret;
 }
@@ -138,89 +137,7 @@ static int fbtft_request_gpios(struct fbtft_par *par)
 
 	return 0;
 }
-#else
-// new GPIO approach lets DT specify linkages
-//https://blog.csdn.net/qq_46604211/article/details/116449891
-static int fbtft_request_one_gpio(struct fbtft_par *par,
-                  const char *name, int index,
-                  struct gpio_desc **gpiop)
-{
-    struct device *dev = par->info->device;
-    struct device_node *node = dev->of_node;
-    int gpio, flags, ret = 0;
-    enum of_gpio_flags of_flags;
-    if (of_find_property(node, name, NULL)) {
-        gpio = of_get_named_gpio_flags(node, name, index, &of_flags);
-        if (gpio == -ENOENT)
-            return 0;
-        if (gpio == -EPROBE_DEFER)
-            return gpio;
-        if (gpio < 0) {
-            dev_err(dev,
-                "failed to get '%s' from DT\n", name);
-            return gpio;
-        }
-         //active low translates to initially low
-        flags = (of_flags & OF_GPIO_ACTIVE_LOW) ? GPIOF_OUT_INIT_LOW :
-                            GPIOF_OUT_INIT_HIGH;
-        ret = devm_gpio_request_one(dev, gpio, flags,
-                        dev->driver->name);
-        if (ret) {
-            dev_err(dev,
-                "gpio_request_one('%s'=%d) failed with %d\n",
-                name, gpio, ret);
-            return ret;
-        }
 
-        *gpiop = gpio_to_desc(gpio);
-        fbtft_par_dbg(DEBUG_REQUEST_GPIOS, par, "%s: '%s' = GPIO%d\n",
-                            __func__, name, gpio);
-    }
-
-    return ret;
-}
-
-static int fbtft_request_gpios(struct fbtft_par *par)
-{
-    int i;
-    int ret;
-
-    ret = fbtft_request_one_gpio(par, "reset-gpios", 0, &par->gpio.reset);
-    if (ret)
-        return ret;
-    ret = fbtft_request_one_gpio(par, "dc-gpios", 0, &par->gpio.dc);
-    if (ret)
-        return ret;
-    ret = fbtft_request_one_gpio(par, "rd-gpios", 0, &par->gpio.rd);
-    if (ret)
-        return ret;
-    ret = fbtft_request_one_gpio(par, "wr-gpios", 0, &par->gpio.wr);
-    if (ret)
-        return ret;
-    ret = fbtft_request_one_gpio(par, "cs-gpios", 0, &par->gpio.cs);
-    if (ret)
-        return ret;
-    ret = fbtft_request_one_gpio(par, "latch-gpios", 0, &par->gpio.latch);
-    if (ret)
-        return ret;
-    for (i = 0; i < 16; i++) {
-        ret = fbtft_request_one_gpio(par, "db-gpios", i,
-                         &par->gpio.db[i]);
-        if (ret)
-            return ret;
-        ret = fbtft_request_one_gpio(par, "led-gpios", i,
-                         &par->gpio.led[i]);
-        if (ret)
-            return ret;
-        ret = fbtft_request_one_gpio(par, "aux-gpios", i,
-                         &par->gpio.aux[i]);
-        if (ret)
-            return ret;
-    }
-
-    return 0;
-}
-#endif
 #ifdef CONFIG_FB_BACKLIGHT
 static int fbtft_backlight_update_status(struct backlight_device *bd)
 {
